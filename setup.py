aaffcebc7a298e13c7b80306011b9d81be022ea7
bnolton <brian.nolton@outlook.com>
2024-10-30 20:14:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='booklover',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/bnolton/Booklover',
+    author='Brian Nolton',
+    author_email='frv3fp@virginia.edu',
+    license='MIT',
+    packages=['booklover']
+)
